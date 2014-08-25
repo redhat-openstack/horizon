448aa4a1349fbb8dff054e78f6fac7eb0370bf46
Radomir Dopieralski <openstack@sheep.art.pl>
2014-08-25 10:58:39 +0200

    Unbundle bootstrap and use xstatic version
    This uses the xstatic-packaged versions of bootstrap and bootstrap-datepicker.
    It requires newer versions of pyscss and django-pyscss, as there were some
    bugs in there that were just fixed upstream.
    
    Change-Id: I0a7660d29fecde915eb46916453a9ea96296798d
    Implements: bluperint remove-javascript-bundling
    Closes-bug: #1375883
    
    Conflicts:
    	horizon/test/settings.py
    	openstack_dashboard/settings.py
    	openstack_dashboard/static/bootstrap/scss/bootstrap/_tables.scss
    	openstack_dashboard/static/dashboard/scss/horizon.scss
    	requirements.txt
    	run_tests.sh

--- horizon/test/settings.py
+++ horizon/test/settings.py
@@ -26,6 +26,8 @@ import xstatic.main
 import xstatic.pkg.angular
 import xstatic.pkg.angular_cookies
 import xstatic.pkg.angular_mock
+import xstatic.pkg.bootstrap_datepicker
+import xstatic.pkg.bootstrap_scss
 import xstatic.pkg.d3
 import xstatic.pkg.font_awesome
 import xstatic.pkg.hogan
@@ -74,6 +76,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django_nose',
+    'django_pyscss',
     'compressor',
     'horizon',
     'horizon.test',
@@ -159,6 +162,10 @@ STATICFILES_DIRS = (
         xstatic.main.XStatic(xstatic.pkg.angular_cookies).base_dir),
     ('horizon/lib/angular',
         xstatic.main.XStatic(xstatic.pkg.angular_mock).base_dir),
+    ('horizon/lib/bootstrap_datepicker',
+        xstatic.main.XStatic(xstatic.pkg.bootstrap_datepicker).base_dir),
+    ('bootstrap',
+        xstatic.main.XStatic(xstatic.pkg.bootstrap_scss).base_dir),
     ('horizon/lib',
         xstatic.main.XStatic(xstatic.pkg.d3).base_dir),
     ('horizon/lib',
